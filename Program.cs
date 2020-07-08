cb385e41a78494ee179439931cd67c71f3b37f36
Entra21 <entra21@proway.treina>
2020-07-07 21:08:24 -0300

    priemiro-commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace primeira_aula
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myText = "meu texto";
+            var myNumber = 8;
+            var myPrice = 4.099;
+            var myBoolean = false;
+
+            Console.WriteLine("Você é maior de idade");
+            var result = Console.ReadLine();
+
+            myBoolean = result ==  "sim"; /* '==' comparação*/
+
+            Console.WriteLine(myText);
+            Console.WriteLine(myBoolean);
+            Console.WriteLine(myBoolean);
+
+
+
+        }
+    }
+}
